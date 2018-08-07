31a1887d3381cfe0f05371697a64ad6d70cd9845
Artur Ponomarenko <ponomarenkoartur@protonmail.com>
2018-08-07 22:18:52 +0300

    lesson1

---
+++ app-swoosh/app-swoosh/Views/BorderedButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderedButton.swift
+//  app-swoosh
+//
+//  Created by Artur on 8/7/18.
+//  Copyright © 2018 Artur. All rights reserved.
+//
+
+import UIKit
+
+class BorderedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
