f5ca2c97ee2c9b6485109170f1bd284730b747de
Artur Ponomarenko <ponomarenkoartur@protonmail.com>
2018-08-07 22:19:51 +0300

    lesson 1

--- app-swoosh/app-swoosh/Views/BorderedButton.swift
+++ app-swoosh/app-swoosh/Views/BorderedButton.swift
@@ -7,15 +7,13 @@
 //
 
 import UIKit
-
 class BorderedButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 2.0
+        
     }
-    */
 
 }
